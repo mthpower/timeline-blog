500f756e72ffd246b04426386f538b74c1590776
Matthew Power <matthew.power@hogarthww.com>
2014-09-25 14:30:01 +0100

    initial article & tag models

--- timeline/blog/models.py
+++ timeline/blog/models.py
@@ -1,3 +1,22 @@
-from django.db import models
+# -*- coding: utf8 -*-
 
-# Create your models here.
+from django.db import models
+from django.utils import timezone
+
+
+class Article(models.Model):
+    author = models.CharField(max_length=512)
+    created_on = models.DateTimeField(default=timezone.now)
+    publication_date = models.DateField(null=True)
+    title = models.CharField(max_length=512)
+    tag = models.ManyToManyField('blog.Tag', blank=True, null=True)
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=128, blank=False)
+
+    class Meta:
+        ordering = ['name']
+
+    def __unicode__(self):  # pragma: no cover
+        return '{0}'.format(self.name)
