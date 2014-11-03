789a51b5a343af3bd325050049a5ce4f4e011495
Matthew Power <matthew.power@hogarthww.com>
2014-11-03 18:29:29 +0000

    oops

--- timeline/timeline/settings.py
+++ timeline/timeline/settings.py
@@ -98,7 +98,7 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 # Media files (User uploaded files)
 # WARNING: This is (probably) not a sensible configuration for prod.
 
-MEDIA_ROOT = (os.path.join(BASE_DIR, 'static/media'),)
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/media')
 MEDIA_URL = '/static/media/'
 
 # Template Directories
