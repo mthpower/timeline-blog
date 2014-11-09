08116b336d01397998f431ece04fad9b12d7c771
Matthew Power <matthew.power@hogarthww.com>
2014-11-09 23:18:09 +0000

    Basic fadetoggle. Should be able to implement a nicer fade when the time comes.

--- timeline/static/js/app.js
+++ timeline/static/js/app.js
@@ -22,31 +22,5 @@ define(function (require) {
             timeline.render();
     });
 
-    $(document).click(function(event) {
-        var target = $( event.target );
-
-        // Check to see if the target is the clickpoint.
-        if (target.parent().is( ".clickpoint" )) {
-        var time = 1000;
-
-        // Iterate through each element in article div and increase animation
-        // time for each one.
-        var articleElems = target.parent().siblings();
-        if ($(articleElems[articleElems.length -1]).css('visibility') == 'hidden') {
-            $(articleElems).each(function() {
-                $(this).css({opacity: 0.0, visibility: "visible"}).animate({opacity: 1.0}, time);
-                time = time + 1500;
-            });
-        } else if ($(articleElems[articleElems.length -1]).css('visibility') == 'visible'){
-            $(articleElems).each(function() {
-                $(this).css({opacity: 1.0, visibility: "hidden"}).animate({opacity: 0.0}, time);
-                time = time + 1500;
-            });
-        }
-        // Prevent default event -- may not need this, try to see
-        return( false );
-      }
-    });
-
     return {};
 });
